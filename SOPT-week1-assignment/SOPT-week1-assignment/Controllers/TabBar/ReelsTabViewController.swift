74eebc53aa2dcb7a4b4b8f12dec757f7580ddbee
MINJAE KIM <ffalswo2@naver.com>
2022-04-18 02:07:59 +0900

    ➕[ADD] : TabBar의  각Content View당 VC 생성 (#3)

---
+++ SOPT-week1-assignment/SOPT-week1-assignment/Controllers/TabBar/ReelsTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReelsTabViewController.swift
+//  SOPT-week1-assignment
+//
+//  Created by 김민재 on 2022/04/18.
+//
+
+import UIKit
+
+class ReelsTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SOPT-week1-assignment/SOPT-week1-assignment/Controllers/TabBar/ShopTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShopTabViewController.swift
+//  SOPT-week1-assignment
+//
+//  Created by 김민재 on 2022/04/18.
+//
+
+import UIKit
+
+class ShopTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
