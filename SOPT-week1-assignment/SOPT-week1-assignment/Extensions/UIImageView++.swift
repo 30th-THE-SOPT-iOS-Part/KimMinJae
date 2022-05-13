4ff7e263fa334fe15289826b9a70c9e9fbab1bc8
MINJAE KIM <ffalswo2@naver.com>
2022-05-14 01:11:33 +0900

    #- ➕[ADD] : UIImageView+ extension 추가 (#7)
    urlString에서 이미지 불러오는 함수 추가

--- SOPT-week1-assignment/SOPT-week1-assignment/Extensions/UIImageView++.swift
+++ SOPT-week1-assignment/SOPT-week1-assignment/Extensions/UIImageView++.swift
@@ -10,13 +10,13 @@ import UIKit
 var imageCache = NSCache<AnyObject, AnyObject>()
 extension UIImageView {
     
-    func load(urlString: String) {
+    func load(urlString: String, newWidth: CGFloat) {
         guard let url = URL(string: urlString) else { return }
         DispatchQueue.global().async { [weak self] in
             if let data = try? Data(contentsOf: url) {
                 if let image = UIImage(data: data) {
                     DispatchQueue.main.async {
-                        self?.image = image
+                        self?.image = image.resize(newWidth: UIScreen.main.bounds.width)
                     }
                 }
             }
